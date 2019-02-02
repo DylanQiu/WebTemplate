9485ba6e0c732207a4d58722c79565f6b1af925d
dylan.Qiu <bojia.qiu@mail.mcgill.ca>
2019-02-02 09:02:12 -0500

    Todo3 1.0

--- TODO_jQuery/todo.js
+++ TODO_jQuery/todo.js
@@ -1,1 +1,21 @@
+// completed
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("completed");
+});
 
+// delete on X
+$("ul").on("click", "span", function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+// add new event
+$("input[type='text']").keypress(function(event){
+    if (event.which === 13) {
+        var todoText = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span><i class='fas fa-trash'></i></span> " + todoText + "</li>");
+    }
+});
